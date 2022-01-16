4dd55268a5c8c09ed20e681ada4fc4f74d29320d
VincentChieu <97499311+VincentChieu@users.noreply.github.com>
2022-01-15 17:43:03 -0800

    Bounds

--- Auto_Chess/Assets/Scripts/Snap.cs
+++ Auto_Chess/Assets/Scripts/Snap.cs
@@ -22,6 +22,26 @@ public class Snap : MonoBehaviour
             25,
             (Mathf.Floor(this.transform.position.z / 100) * 100) + 50);
 
+        // x Bounds
+        if (position.x < 0)
+        {
+            position.x = 0;
+        }
+        if (position.x > 950)
+        {
+            position.x = 950;
+        }
+
+        // z Bounds
+        if (position.z < 50)
+        {
+            position.z = 50;
+        }
+        if (position.z > 950)
+        {
+            position.z = 950;
+        }
+
         this.transform.position = position;
     }
 }
